045eaf3c544be6500f8d825c56aeda956c693720
hieiedsilva-cpu <hieiedsilva@gmail.com>
2025-09-02 15:54:14 -0300

    Cria a estrutura inicial da aplicação React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Meu App React está funcionando!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Se você tiver um arquivo de CSS global
 import App from './App'; // Importa o componente principal
 
 const root = ReactDOM.createRoot(
